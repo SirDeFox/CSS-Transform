f472e161849e6c98a3d24d53faef12ea2605e6f6
SirDeFox <109417394+SirDeFox@users.noreply.github.com>
2022-10-25 21:54:29 +0300

    Delete CSS Transform directory

--- CSS Transform/js/script.js
+++
@@ -1,7 +0,0 @@
-let theme = document.querySelector('.dark-theme');
-let themeButton = document.querySelector('.theme-button');
-
-themeButton.onclick = function () {
-    theme.classList.toggle('light-theme');
-    theme.classList.toggle('dark-theme');
-}
